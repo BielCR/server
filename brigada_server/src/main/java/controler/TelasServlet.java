8a808bacfebad8115a2e389e87ad4f1520f1b165
BielCR <gabrielcr18@outlook.com>
2021-03-19 16:20:54 -0300

    Servlets parcialmente funcionando aul 9

---
+++ brigada_server/src/main/java/controler/TelasServlet.java
@@ -0,0 +1,89 @@
+package controler;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.SQLException;
+import tools.FabricaConexao;
+
+/**
+ *
+ * @author gabri
+ */
+public class TelasServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet TelasServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>teste</h1>");
+            
+            Connection conn = FabricaConexao.criaConexao();
+            try {
+                if(!conn.isClosed()){
+                    out.println("<h1>Conexão funcionando corretamente</h1>");
+                }else{
+                    out.println("<h1>Falha ao conectar</h1>");
+                }
+            } catch (SQLException ex) {
+               out.println("<h1>Erro de conexão</h1>");
+            }
+            
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
